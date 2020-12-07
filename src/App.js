85d8665d72c11c444695eff7ad3d3812ba299a7c
bestedicte <benedictevikvam@hotmail.com>
2020-12-07 14:58:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from "./components/navigation/Navigation"
+
+function App() {
+  return (
+    <div className="App">
+  <Navigation />
+  {/* {{/ < Logo />
+  <ImageLinkForm />
+  <Recognition />  }}
+ */}    </div>
+  );
+}
+
+export default App;
