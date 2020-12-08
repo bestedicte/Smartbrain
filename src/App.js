af8bedad72c0631f3f610311913c2b07b5b4bcea
bestedicte <benedictevikvam@hotmail.com>
2020-12-08 11:48:51 +0100

    added more css and visuals

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import "./App.css";
-import Particles from "react-particles-js";
+import Particles from "react-particles-js"
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
 import Rank from "./components/rank/Rank";
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 500,
+      density: {
+        enable: true,
+        value_area: 100,
+      },
+    },
+  },}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles
-          params={{
-            polygon: {
-              enable: true,
-              type: "inside",
-              move: {
-                radius: 10,
-              },
-              url: "path/to/svg.svg",
-            },
-          }}
-        />
-
+        <Particles className="particles"
+          params={{particlesOptions}}
+          />
         <Navigation />
         <Logo />
         <Rank />
